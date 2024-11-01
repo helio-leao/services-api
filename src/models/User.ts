0e673be9307116f15b532c20985d639d6ab0be7c
Helio Leao <hmtleao@hotmail.com>
2024-10-31 22:11:28 -0300

    add timestamps to user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,25 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  gender: String,
-  contact: {
-    celphone: String,
-    email: String,
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    gender: String,
+    contact: {
+      celphone: String,
+      email: String,
+    },
+    address: {
+      street: String,
+      district: String,
+      number: String,
+      complement: String,
+      zip: String,
+    },
+    service: {
+      title: String,
+      description: String,
+      category: { type: mongoose.Types.ObjectId, ref: "ServiceCategory" },
+      subcategory: { type: mongoose.Types.ObjectId, ref: "ServiceSubcategory" },
+    },
   },
-  address: {
-    street: String,
-    district: String,
-    number: String,
-    complement: String,
-    zip: String,
-  },
-  service: {
-    title: String,
-    description: String,
-    category: { type: mongoose.Types.ObjectId, ref: "ServiceCategory" },
-    subcategory: { type: mongoose.Types.ObjectId, ref: "ServiceSubcategory" },
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
